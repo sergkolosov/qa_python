e87f145269049d329762e5fc347214dbbf86c08a
Sergey Kolosov <serikbay1971@yandex.ru>
2023-05-14 14:08:39 +0400

    Added a fixture with autouse=True. Creates an instance of a class with three books

--- tests.py
+++ tests.py
@@ -1,24 +1,22 @@
+import pytest
+
 from main import BooksCollector
 
-# класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
-# обязательно указывать префикс Test
+
 class TestBooksCollector:
 
-    # пример теста:
-    # обязательно указывать префикс test_
-    # дальше идет название метода, который тестируем add_new_book_
-    # затем, что тестируем add_two_books - добавление двух книг
-    def test_add_new_book_add_two_books(self):
+    @pytest.fixture(autouse=True)
+    def collector(self):
         # создаем экземпляр (объект) класса BooksCollector
-        collector = BooksCollector()
+        self.collector = BooksCollector()
+        # # добавляем две книги
+        self.collector.add_new_book('Гордость и предубеждение и зомби')
+        self.collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        self.collector.add_new_book('Над пропастью во ржи')
 
-        # добавляем две книги
-        collector.add_new_book('Гордость и предубеждение и зомби')
-        collector.add_new_book('Что делать, если ваш кот хочет вас убить')
+        return self.collector
 
-        # проверяем, что добавилось именно две
+    def test_add_new_book_add_two_books(self):
+        # проверяем, что добавилось именно две книги
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
-
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        assert len(self.collector.get_books_rating()) == 2
